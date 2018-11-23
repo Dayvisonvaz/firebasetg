afc0837bb2dcb6696fad6720dc1ead6d143422a8
Dayvison <dayvison.vaz@fatecitapetininga.edu.br>
2018-11-23 20:15:05 -0200

    Initial commit

---
+++ app/src/main/java/com/view/aluno.java
@@ -0,0 +1,31 @@
+package com.view;
+
+public class aluno {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    private String nome, observacao;
+}
---
+++ app/src/main/java/com/view/telefone.java
@@ -0,0 +1,24 @@
+package com.view;
+
+public class telefone {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    private String numero;
+
+}
